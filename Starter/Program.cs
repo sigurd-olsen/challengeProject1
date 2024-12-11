62dbaee8fae43dcbaccc5aecf6787fe73c35908f
sigurd-olsen <sigurddaaolsen@gmail.com>
2024-12-11 10:14:18 +0100

    La til tomme variabler for oppgaven

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -33,7 +33,7 @@ string currentStudentLetterGrade = "";
 
 // display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
 /*
 The outer foreach loop is used to:
@@ -64,6 +64,12 @@ foreach (string name in studentNames)
     decimal currentStudentGrade = 0;
 
     int gradedAssignments = 0;
+
+    decimal currentStudentExamScore = 0;
+
+    decimal currentStudentExtraCredit = 0;
+
+    decimal currentStudentExtraPoints = 0;
 
     /* 
     the inner foreach loop sums assignment scores
@@ -124,7 +130,7 @@ foreach (string name in studentNames)
     // Student         Grade
     // Sophia:         92.2    A-
     
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCredit} ({currentStudentExtraPoints} pts)");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
